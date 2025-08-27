17cdc14dc80748ff83e485ff33e2dad75827904e
flitme <140435421+flitme@users.noreply.github.com>
2025-08-27 14:53:58 +0500

    Update bot_f.py

--- kadinsky/bot_f.py
+++ kadinsky/bot_f.py
@@ -5,7 +5,6 @@ from discord import app_commands
 from api import config
 from kad import name
 
-#import subprocess
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents = discord.Intents.all(), help_command=None, test_guilds=config.config['GUILD_ID'])# айди вашего сервера в квадратные скобки
@@ -24,25 +23,20 @@ async def help(ctx):
 
 @bot.tree.command(name="discr")
 @app_commands.describe(
-    discr='много букв',
+    discr='описание',
 )
 async def add(interaction: discord.Interaction, discr: str):
     """Adds two numbers together."""
     await interaction.response.send_message(f'{discr}')
 
-    #print(a)
-    # libr = {
-    #     'Description': f'{discr}',
-    # }
-    # a = libr['Description']
     cahe = discr
     name(cahe)
-    #os.startfile(r'C:\Users\flitme\Desktop\yolov20\kad_test\kad.py')
-    #subprocess.Popen(r'C:\Users\flitme\Desktop\yolov20\kadinsky\kad.py', shell=True)
+
     print(a)
 
     channel = bot.get_channel(1031102919598161924)
     await channel.send(file=discord.File('copy.jpg'))
     os.remove("copy.jpg")
+
 
 bot.run(config.config['TOKEN'])
